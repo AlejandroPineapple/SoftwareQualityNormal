1d7b2df9851f664a4f05f4beace36cbe385355a0
AlejandroPineapple <157551222+AlejandroPineapple@users.noreply.github.com>
2024-12-05 17:57:15 -0600

    Update UserServiceUnitTest.java
    qhora si funciona

--- src/test/java/com/mayab/quality/loginunittest/service/UserServiceUnitTest.java
+++ src/test/java/com/mayab/quality/loginunittest/service/UserServiceUnitTest.java
@@ -256,7 +256,7 @@ class UserServiceUnitTest {
 
 	    // Verify
 	    assertThat(result, is(not(empty())));
-	    assertThat(result.size(), is(246556)); // verify encontramos 2 usuarios
+	    assertThat(result.size(), is(2)); // verify encontramos 2 usuarios
 	}
 
 
